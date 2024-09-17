824062e01d518244783b7abd51a227e06abd1a2e
Dani1lau <soydanielra16@gmail.com>
2024-09-17 18:46:59 -0500

    .

--- server/routes/usuario.routes.js
+++ server/routes/usuario.routes.js
@@ -4,8 +4,8 @@ import UsuarioController from "../controllers/usuario.controller.js";
 const router = Router();
 
 router.get( '/api/usuario', UsuarioController.getUsuarios )
-router.get('/api/usuario/tipoDoc/:tipoDoc/documento/:documento/nombre/:nombre?', UsuarioController.getUsuario);
-router.get('/api/perfil/:id', UsuarioController.getPerfil);
+router.get('/api/usuario/tipoDoc/:tipoDoc/documento/:documento/nombre/:nombre?', UsuarioController.getUsuario)
+router.get('/api/perfil/:id', UsuarioController.getPerfil)
 router.put( '/api/usuario/:id', UsuarioController.putUsuario )
 router.post( '/api/usuario', UsuarioController.postUsuario )
 router.delete( '/api/usuario/:id', UsuarioController.inactivarUsuario )
